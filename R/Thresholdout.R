b78e2ed449b9c6f7c37b1789090f909a84bef14a
Bobbitt <bobbits1@hmcorp.local>
2018-04-17 09:58:41 -0500

    Update Thresholdout with R6 style and provide active access to threshold and sigma.

--- R/Thresholdout.R
+++ R/Thresholdout.R
@@ -7,34 +7,35 @@
 #'
 #' @section Methods:
 #' \describe{
-#'   \item{\code{$new(Threshold, Sigma, Budget)}}{Creates a new \code{Thresholdout} object.}
+#'   \item{\code{$new(threshold, sigma, budget)}}{Creates a new \code{Thresholdout} object.}
 #'   \item{\code{$query(train_val, holdout_val)}}{Sends a query to the holdout dataset.}
 #' }
 #'
 #' @section Actives:
 #' \describe{
-#'   \item{\code{$Threshold}}{gets the Threshold value.}
-#'   \item{\code{$Sigma}}{gets the Sigma value.}
-#'   \item{\code{$Budget}}{gets the remaining Budget value.}
-#'   \item{\code{$Record}}{gets the Record of previous queries.}
+#'   \item{\code{$threshold}}{gets the threshold value.}
+#'   \item{\code{$sigma}}{gets the sigma value.}
+#'   \item{\code{$budget}}{gets the remaining budget value.}
+#'   \item{\code{$record}}{gets the record of previous queries.}
 #' }
 #'
 #' @examples
 #' library(rThresholdout)
-#' Thresholdout_Obj <- Thresholdout$new(Threshold = 0.04, Sigma = 0.01, Budget = 1000)
+#' Thresholdout_Obj <- Thresholdout$new(threshold = 0.04, sigma = 0.01, budget = 1000)
 #' Thresholdout_Obj$query(train_val = c(0.06, 0.07), holdout_val = c(0.05, 0.08))
 #'
 #' @importFrom stats runif
 #' @importFrom R6 R6Class
+#' @importFrom assertive.types assert_is_numeric
 #' @export
 
 Thresholdout <- R6Class(
   classname = "Thresholdout",
   public = list(
-    initialize = function(Threshold, Sigma, Budget) {
-      private$threshold <- Threshold
-      private$sigma <- Sigma
-      private$budget <- Budget
+    initialize = function(threshold, sigma, budget) {
+      private$..threshold <- threshold
+      private$..sigma <- sigma
+      private$..budget <- budget
     },
     query = function(train_val, holdout_val) {
       if (length(train_val) != length(holdout_val)) {
@@ -42,48 +43,54 @@ Thresholdout <- R6Class(
       } else {
         N <- length(train_val)
       }
-      if (private$budget < 1) {
+      if (private$..budget < 1) {
         message("Budget exhausted")
       } else {
-        private$noise_update(n = N)
-        threshold_ind <- abs(train_val - holdout_val) > private$threshold + private$gamma + private$eta
-        if (private$budget - sum(threshold_ind) < 1) {
+        private$..noise_update(n = N)
+        threshold_ind <- abs(train_val - holdout_val) > private$..threshold + private$..gamma + private$..eta
+        if (private$..budget - sum(threshold_ind) < 1) {
           message("Budget is not enough for current query")
         }
-        private$budget <- private$budget - sum(threshold_ind)
-        result <- holdout_val + private$xi
+        private$..budget <- private$..budget - sum(threshold_ind)
+        result <- holdout_val + private$..xi
         result[!threshold_ind] <- train_val[!threshold_ind]
-        private$record <- append(private$record, list(data.table::data.table(matrix(result, nrow = 1))))
+        private$..record <- append(private$..record, list(data.table::data.table(matrix(result, nrow = 1))))
         return(result)
       }
     }
   ),
   private = list(
-    threshold = NULL,
-    sigma = NULL,
-    budget = NULL,
-    xi = NULL,
-    gamma = NULL,
-    eta = NULL,
-    record = list(),
-    noise_update = function(n) {
-      private$xi <- rLaplace(n = n, s = 1 * private$sigma)
-      private$gamma <- rLaplace(n = n, s = 2 * private$sigma)
-      private$eta <- rLaplace(n = n, s = 4 * private$sigma)
+    ..threshold = NULL,
+    ..sigma = NULL,
+    ..budget = NULL,
+    ..xi = NULL,
+    ..gamma = NULL,
+    ..eta = NULL,
+    ..record = list(),
+    ..noise_update = function(n) {
+      private$..xi <- rLaplace(n = n, s = 1 * private$..sigma)
+      private$..gamma <- rLaplace(n = n, s = 2 * private$..sigma)
+      private$..eta <- rLaplace(n = n, s = 4 * private$..sigma)
     }
   ),
   active = list(
-    Threshold = function() {
-      private$threshold
+    threshold = function(value) {
+      if(!missing(value)) {
+        assert_is_numeric(value)
+        private$..threshold <- value
+      } else private$..threshold
     },
-    Sigma = function() {
-      private$sigma
+    sigma = function(value) {
+      if(!missing(value)) {
+        assert_is_numeric(value)
+        private$..sigma <- value
+      } else private$..sigma
     },
-    Budget = function() {
-      private$budget
+    budget = function() {
+      private$..budget
     },
-    Record = function() {
-      data.table::rbindlist(private$record)
+    record = function() {
+      data.table::rbindlist(private$..record)
     }
   ),
   inherit = NULL,
@@ -93,7 +100,6 @@ Thresholdout <- R6Class(
   lock_class = TRUE,
   cloneable = FALSE
 )
-
 
 rLaplace <- function (n = 1, m = 0, s = 1) {
   q <- runif(n)
